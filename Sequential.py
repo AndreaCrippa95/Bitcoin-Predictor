6e6bf15cca6d55d56df99280e5ca537434f4b575
AndreaCrippa95 <80115785+AndreaCrippa95@users.noreply.github.com>
2021-04-20 18:30:33 +0200

    Fixed up Sequential.py

--- Sequential.py
+++ Sequential.py
@@ -1,10 +1,6 @@
 #Use the Sequential Neural Network to predict the price of the Bitcoin
 #Imports
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import pandas_datareader as web
-import datetime as dt
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
@@ -13,7 +9,7 @@ from tensorflow.keras.layers import Dense, Dropout, LSTM
 from Database import df
 #Scaling the data to be between 0 and 1
 scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(df['close'].values.reshape(-1,1))
+scaled_data = scaler.fit_transform(df['Price'].values.reshape(-1,1))
 
 #Days used to predict future price
 days = 60
@@ -45,52 +41,13 @@ model.add(Dense(units=1))
 model.compile(optimizer='adam', loss= 'mean_squared_error')
 model.fit(X_train,y_train,epochs=25,batch_size=32)
 
-#Test model accuracy on existing data
-
-test_start = dt.datetime(2020,1,1)
-test_start = test_start.strftime("%Y-%m-%d")
-
-test_end = dt.datetime.now()
-test_end = test_end.strftime("%Y-%m-%d")
-
-df_test = web.get_data_tiingo(Ticker,test_start,test_end, api_key = ('eef2cf8be7666328395f2702b5712e533ea072b9'))
-
-actual_prices = df_test['close'].values
-tot_df = pd.concat((df['close'],df_test['close']),axis=0)
-
-model_input = tot_df[len(tot_df)-len(df_test)-days:].values
-model_input = model_input.reshape(-1,1)
-model_input = scaler.transform(model_input)
-
-#Make some predictions on the test data
-
-X_test = []
-
-for i in range(days,len(model_input)):
-    X_test.append(model_input[i-days:i,0])
-
-X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
-
-predictions = model.predict(X_test)
-predictions = scaler.inverse_transform(predictions)
-
-#Plot test predictions:
-
-plt.plot(actual_prices, color = 'green', label = 'Actual Price')
-plt.plot(predictions, color = 'orange', label = 'Predicted Price')
-plt.title('Bitcoin Price')
-plt.xlabel('Time')
-plt.ylabel('Price')
-plt.legend()
-plt.show()
-
-
 #Predict next day
 
-real_df = [model_input[len(model_input)+1-days:len(model_input+1),0]]
+prediction = []
+real_df = [scaled_data[len(scaled_data)-(days):len(scaled_data)]]
 real_df = np.array(real_df)
-real_df = np.reshape(real_df,(real_df.shape[0],real_df.shape[1],1))
+real_df = np.append(real_df, prediction)
+real_df = np.reshape(real_df,(1,days,1))
 
 
 prediction = model.predict(real_df)
@@ -98,5 +55,17 @@ prediction = scaler.inverse_transform(prediction)
 
 print(prediction)
 
+prediction = []
+for i in range(days):
+    real_df = [scaled_data[len(scaled_data)-(days-i):len(scaled_data)]]
+    real_df = np.append(real_df, prediction)
+    real_df = np.array(real_df)
+    real_df = np.reshape(real_df, (1, days, 1))
+    pr = model.predict(real_df)
+    prediction.append(pr[0,0])
+    real_df = np.append(real_df,prediction)
 
-
+prediction = np.array(prediction)
+prediction = np.reshape(prediction,(1,days,1))
+results = [scaler.inverse_transform(prediction[i]) for i in range(len(prediction))]
+results = np.array(results)
