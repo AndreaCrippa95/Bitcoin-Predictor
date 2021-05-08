d83f36094c00c29d8abe55a32513de1a7aab701e
FlaviioL <79796347+FlaviioL@users.noreply.github.com>
2021-05-08 13:18:06 +0200

    Dash test2

---
+++ Dashboard/tutorial/app.py
@@ -0,0 +1,53 @@
+import flask
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+path = '/Users/flavio/Documents/GitHub/Bitcoin-Predictor/data/BTC_Historic/bitcoin_price_1week_Test - Test.csv'
+df = pd.read_csv(path)
+df.index = pd.to_datetime(df['Date'])
+df.head()
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+fig = px.line(x=df["Date"], y=df["Close"])
+
+dcc.Dropdown(
+    options=[
+        {'label': 'Neural Network', 'value': 'RNN'},
+        {'label': 'Random_walk', 'value': 'RW'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    value='RNN'
+)
+
+dcc.Dropdown(
+    options=[
+        {'label': 'Neural Network', 'value': 'RNN'},
+        {'label': 'Random_walk', 'value': 'RW'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    value='RNN'
+)
+
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Welcome to our Bitcoin Forecasting Dashboard'),
+    html.Div(children='The forecast in this blog are for general informational purposes only and are not intended to provide specific advice financial advise.'),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+    dcc.Dropdown(id='to define')
+])
+
+
+server = app.server
+if __name__ == '__main__':
+    app.run_server(
+        port=8060,
+        host='0.0.0.0')
---
+++ Dashboard/tutorial/script.sh
@@ -0,0 +1,7 @@
+#To run your dashboard via Terminal
+
+FLASK_APP=app.py
+export FLASK_APP=app.py
+python -m flask run
+
+#you will find examples via https://dash-gallery.plotly.host/Portal/
