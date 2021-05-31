7866ed1f2b7d6a70dfa57c01655565b159a6e34b
FlaviioL <79796347+FlaviioL@users.noreply.github.com>
2021-05-31 13:23:52 +0200

    References without my keys

--- Dashboard/Static/admin/references.py
+++ Dashboard/Static/admin/references.py
@@ -1,6 +1,6 @@
-# Flavio's twitter account, please do change to find yours.
+# Please replace your account for twitter API
 
-API = 'ih1vfWmIgXNN36iUdtkRA8SNd'
-API_secret = 'vZeS0JWBNnaMCLjVVtwEMIFANLTcI5oC1P9N6dpyt4YF4MFPPY'
-Access = '103961010-lpTQz31OQJRrm5HNClnJg52JABCbCgDEuglpEMew'
-Access_secret = 'YxLpi9IcdnydwfCMXecUYZD3z90kYL5WFD8YAH5rUejWm'
+API = 'iiiiiiiiii'
+API_secret = 'iiiiiiiiiiiiiii'
+Access = 'iiiiiiiiiiiiiiiiii'
+Access_secret = 'iiiiiiiiiiiiiii'
